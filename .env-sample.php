a7d25b49f9e12e14c5e0684ff73eb36186c0c50e
imakero <45946863+imakero@users.noreply.github.com>
2022-09-01 11:52:21 +0200

    add readme and env sample

---
+++ .env-sample.php
@@ -0,0 +1,12 @@
+<?php
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'db name' );
+
+/** Database username */
+define( 'DB_USER', 'db user' );
+
+/** Database password */
+define( 'DB_PASSWORD', 'db password' );
+
+/** Database hostname */
+define( 'DB_HOST', 'db host' );
---
+++ README.md
@@ -0,0 +1,3 @@
+# Installation instructions
+1. Clone this repository
+2. Rename `.env-sample.php` to `.env.php` and fill in credentials of your database.
